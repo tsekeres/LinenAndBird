ac9e1ad1908fabb225593b93614e85c6d6847e3b
tsekeres <twsekeres@gmail.com>
2021-08-28 08:12:09 -0500

    Add project files.

---
+++ LinenAndBird/Controllers/HatsController.cs
@@ -0,0 +1,39 @@
+﻿using LinenAndBird.DataAccess;
+using LinenAndBird.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinenAndBird.Controllers
+{
+    [Route("api/hats")]
+    [ApiController]
+    public class HatsController : ControllerBase
+    {
+        HatRepository _repo;
+        public HatsController()
+        {
+            _repo = new HatRepository();
+        }
+        [HttpGet]
+        public List<Hat> GetAllHats()
+        {
+            return _repo.GetAll();
+        }
+
+        [HttpGet("styles/{style}")]
+        public IEnumerable<Hat> GetHatsByStyle(HatStyle style)
+        {
+            return _repo.GetByStyle(style);
+        }
+
+        [HttpPost]
+        public void AddAHat(Hat newHat)
+        {
+            _repo.Add(newHat);
+        }
+    }
+}
---
+++ LinenAndBird/Models/Hat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinenAndBird.Models
+{
+    public class Hat
+    {
+        public Guid Id { get; set; }
+
+        public string Designer { get; set; }
+        public string Color { get; set; }
+        public HatStyle Style { get; set; }
+    }
+
+    public enum HatStyle
+    { 
+        Normal,
+        OpenBack,
+        WideBrim
+    }
+}
