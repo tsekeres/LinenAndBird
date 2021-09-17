51c06987b81264ae03c4d03f07644340dffbbf4d
tsekeres <twsekeres@gmail.com>
2021-09-17 15:59:20 -0500

    adding ado.net info

--- LinenAndBird/DataAccess/BirdRepository.cs
+++ LinenAndBird/DataAccess/BirdRepository.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 
 namespace LinenAndBird.DataAccess
 {
@@ -23,7 +24,32 @@ namespace LinenAndBird.DataAccess
 
         internal IEnumerable<Bird> GetAll()
         {
-            return _birds;
+            using var connection = new SqlConnection("Server=localhost;Database=LinenAndBird;Trusted_Connection=True;");
+
+            connection.Open();
+
+            var cmd = connection.CreateCommand();
+            cmd.CommandText = @"SELECT *
+                                FROM Birds";
+
+            var reader = cmd.ExecuteReader();
+
+            var birds = new List<Bird>();
+
+            while(reader.Read())
+            {
+                //Mapping from the relational model to the object model
+                var bird = new Bird();
+                bird.Id = reader.GetGuid(0);
+                bird.Size = reader["Size"].ToString(); //convert to string to be the right form in our object
+                bird.Type = (BirdType)reader["Type"]; //explicit casting to make sure it knows it should be read as is
+                bird.Color = reader["Color"].ToString();
+                bird.Name = reader["Name"].ToString();
+
+                birds.Add(bird);
+            }
+
+            return birds;
         }
 
         internal void Add(Bird newBird)
@@ -35,7 +61,34 @@ namespace LinenAndBird.DataAccess
 
         internal Bird GetById(Guid birdId)
         {
-            return _birds.FirstOrDefault(bird => bird.Id == birdId);
+            using var connection = new SqlConnection("Server=localhost;Database=LinenAndBird;Trusted_Connection=True;");
+
+            connection.Open();
+
+            var cmd = connection.CreateCommand();
+            cmd.CommandText = @"SELECT *
+                                FROM Birds
+                                WHERE id = @id";
+
+            cmd.Parameters.AddWithValue("id", birdId);
+
+            var reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                var bird = new Bird();
+                bird.Id = reader.GetGuid(0);
+                bird.Size = reader["Size"].ToString(); //convert to string to be the right form in our object
+                bird.Type = (BirdType)reader["Type"]; //explicit casting to make sure it knows it should be read as is
+                bird.Color = reader["Color"].ToString();
+                bird.Name = reader["Name"].ToString();
+
+                return bird;
+            }
+
+            return null;
+
+            //return _birds.FirstOrDefault(bird => bird.Id == birdId);
         }
     }
 }
